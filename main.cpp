c8657a7389fb268bb705ce712bbff28c7b8fb6df
Yuqin Zhao <yz4131@columbia.edu>
2022-11-18 10:41:12 -0500

    11.18

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 class Save_Reload {
     public:
         bool gameSave(std::string add, std::string name, int length, int height);
+        std::vector<std::string> listGameHistory(std::string add);
+        void gameReload(std::string add, std::string name);
 };
 
 bool Save_Reload::gameSave(std::string add, std::string name, int length, int height) { 
@@ -33,17 +35,29 @@ bool Save_Reload::gameSave(std::string add, std::string name, int length, int he
     return true;
 }
 
+std::vector<std::string> Save_Reload::listGameHistory(std::string add) {
+    std::vector<std::string> result;
+    for (const auto & entry : std::filesystem::directory_iterator(add)){
+        std::string records = std::string(entry.path());
+        
+        // process address
+        records = records.substr(add.length());
+        // ignore .DS_Store file
+        if (records == ".DS_Store")
+            continue;
+        for (int i=0; i<4; i++)
+            records.pop_back();
+        
+        result.push_back(records);
+    }
+    return result;    
+}
+
 
 int main() {
     Save_Reload obj;
-    std::cout<< obj.gameSave("./gameHistory/", "game2", 1, 1);
-    /*
-    std::ofstream fw("./test.txt", std::ofstream::out);
-    if (fw.is_open()) {
-        fw << "abc" << "\n";
-        fw << "a";
-        fw.close();
+    for (std::string r : obj.listGameHistory("./gameHistory/")) {
+        std::cout << r << "\n";
     }
-    */
 }
 
